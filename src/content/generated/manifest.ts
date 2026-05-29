dbfa841ff40e71bd6fb27a082ea7704e4e6f6534
ABM Prottoy <abmlabs@outlook.com>
2026-05-30 03:49:04 +0600

    Add social preview image

--- src/content/generated/manifest.ts
+++ src/content/generated/manifest.ts
@@ -7,7 +7,7 @@ export const manifest = {
   "sourceDescription": "Declarative UI framework for native Windows desktop apps in pure C#.",
   "repoUrl": "https://github.com/microsoft/microsoft-ui-reactor",
   "docsDir": "docs/guide",
-  "generatedAt": "2026-05-29T21:36:44.333Z",
+  "generatedAt": "2026-05-29T21:48:45.688Z",
   "nav": [
     {
       "title": "Home",
